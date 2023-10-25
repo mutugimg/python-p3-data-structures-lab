d73efdbf9177c69fe1f9e88d0598fdc10edf9401
Mark Mutugi <mutugimg@gmail.com>
2023-10-25 17:14:53 +0300

    Completed assignment

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,63 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    result = [food["name"] for food in spicy_foods]
+    # print(result)
+    return result
+get_names(spicy_foods)
+    
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    heat_level = [ item for item in spicy_foods if item ["heat_level"] > 5]
+    # print(heat_level)
+    return heat_level
+get_spiciest_foods(spicy_foods)
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for spice in spicy_foods:
+        print(f"{spice['name']} ({spice['cuisine']}) | Heat Level: {'🌶' * spice['heat_level']}")
+    return spice
+# print_spicy_foods(spicy_foods)
+pass
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if cuisine == food["cuisine"]:
+            return food
+    return None
+
+# print(get_spicy_food_by_cuisine(spicy_foods, "American"))
+
+    # food_by_cuisine = [food for food in spicy_foods if cuisine == food["cuisine"]]
+    # return food_by_cuisine[0] if food_by_cuisine else None
+# print(get_spicy_food_by_cuisine(spicy_foods, "American"))
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = [
+        food for food in spicy_foods if food.get("heat_level") > 5]
+    print_spicy_foods(spiciest_foods)
+
+print_spiciest_foods(spicy_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_level_sum = sum([food["heat_level"] for food in spicy_foods])
+    heat_level_foods= len(spicy_foods)
+    avrg_heat_level = int(heat_level_sum/heat_level_foods)
+    print(avrg_heat_level)
+    return avrg_heat_level
+   
+get_average_heat_level(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.extend([spicy_food])
+    return spicy_foods
+
+spicy_food = {
+    'name': 'Griot',
+    'cuisine': 'Haitian',
+    'heat_level': 10,
+}
+
+new_spicy_foods = create_spicy_food(spicy_foods, spicy_food)
+print(new_spicy_foods[3])
